1a1581a62b30828377039be92f8a0bdda9ae93b7
BrianLevin123 <93602751+BrianLevin123@users.noreply.github.com>
2022-03-11 09:56:09 -0500

    set up starter and firebase files

---
+++ whatsapp/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD96XLBrhuXDUc5u2XJToRBte49XNRkKAo",
+    authDomain: "whatsapp-clone-a5549.firebaseapp.com",
+    projectId: "whatsapp-clone-a5549",
+    storageBucket: "whatsapp-clone-a5549.appspot.com",
+    messagingSenderId: "793988342065",
+    appId: "1:793988342065:web:4ca92c54da46d270035010"
+  };
