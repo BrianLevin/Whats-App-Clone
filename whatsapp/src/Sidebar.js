c3f733abd964afb7d55d7c6e489aeef8e63da1a3
BrianLevin123 <93602751+BrianLevin123@users.noreply.github.com>
2022-03-14 11:12:49 -0400

    set up the sidebar bar and styled it

--- whatsapp/src/Sidebar.js
+++ whatsapp/src/Sidebar.js
@@ -1,17 +1,36 @@
 import React from 'react';
+import DonutLargeIcon from '@mui/icons-material/DonutLarge';
+import ChatIcon from '@mui/icons-material/Chat';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import {Avatar, IconButton } from "@material-ui/core";
+import { SearchOutlined } from "@material-ui/icons";
 import "./Sidebar.css";
 
 function Sidebar() {
   return (
-    <div className= "sidebar"><h1>I am a sidebar</h1>
+    <div className= "sidebar">
     <div className="sidebar__header">
+    <Avatar src="https://avatars1.githubusercontent.com/u/54590754?s=460&u=963670b8242d936102baf79974c98a5f27a06041&v=4" />
 
 <div className= "sidebar__headerRight">
-    
+<IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
 </div>
-
     </div>
-    
+
+     <div className="sidebar__search">
+     <div className="sidebar__searchContainer">
+          <SearchOutlined />
+          <input placeholder="Search or start new chat" type="text" />
+        </div>
+       </div>
     </div>
   )
 }
