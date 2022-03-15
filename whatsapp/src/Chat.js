79587ba56ee13ea2fea80e7c06d86b255740eb6f
BrianLevin123 <93602751+BrianLevin123@users.noreply.github.com>
2022-03-15 14:40:27 -0400

    added chat and styling functioanlity

--- whatsapp/src/Chat.js
+++ whatsapp/src/Chat.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Avatar, IconButton } from "@material-ui/core";
+import MicIcon from '@mui/icons-material/Mic';
 import {
   AttachFile,
   InsertEmoticon,
@@ -29,9 +30,41 @@ function Chat() {
           </IconButton>
         </div>
     </div>
-    
+    <div className="chat__body">
+        {messages.map((message, i) => {
+          return (
+            <p
+              key={i}
+              className={`chat__message ${
+                message.received && "chat__reciever"
+              }`}
+            >
+              <span className="chat__name">{message.name}</span>
+              {message.message}
+              <span className="chat__timestamp">{message.timestamp}</span>
+            </p>
+          );
+        })}
+      </div>
+        <div className="chat__footer">
+        <InsertEmoticon />
+        <form>
+          <input
+            value={input}
+            onChange={(e) => {
+              setInput(e.target.value);
+            }}
+            type="text"
+            placeholder="Type a message"
+          />
+          <button onClick={sendMessage} type="submit">
+            Send a message
+          </button>
+        </form>
+        <MicIcon />
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Chat
