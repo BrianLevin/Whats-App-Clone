a5b948b554a7523209d5aa563c2e669a9fc70698
BrianLevin123 <93602751+BrianLevin123@users.noreply.github.com>
2022-03-17 13:42:37 -0400

    added db messages file and routes

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,16 @@
 // importing
 import express from "express";
 import mongoose from "mongoose";
+import Messages from "./dbMessages.js";
 
 // app cponfig
 const app = express();
 const port = process.env.PORT || 9000;
 
 // middle ware
-
+// middleware
+app.use(express.json());
+// app.use(cors());
 // DB config
 
 const connection_url= 'mongodb+srv://admin:UoTMZ01bpZzDRTx1@cluster0.myidr.mongodb.net/WhatsAppDB?retryWrites=true&w=majority';
@@ -17,7 +20,28 @@ mongoose.connect(connection_url, {
 // ????
 
 // api roputes
-app.get('/',(req,res) => res.status(200).send("Hello World") )
+app.get('/',(req,res) => res.status(200).send("Hello World") );
+
+app.get("/messages/sync", (req, res) => {
+    Messages.find((err, data) => {
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        res.status(200).send(data);
+      }
+    });
+  });
+  
+  app.post("/messages/new", (req, res) => {
+    const dbMessage = req.body;
+    Messages.create(dbMessage, (err, data) => {
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        res.status(201).send(data);
+      }
+    });
+  });
 
 // listener
 app.listen(port,() => console.log(`listening on local host: ${port}`));
